b56a7603919aa04e775d864ee852facf1121674a
ansehun <ansehoon1999@gmail.com>
2025-08-05 11:59:13 +0900

    refactor: change dialog design

--- feature/device-list/src/main/java/com/luxrobo/device_list/ui/Dialog.kt
+++ feature/device-list/src/main/java/com/luxrobo/device_list/ui/Dialog.kt
@@ -1,6 +1,7 @@
 package com.luxrobo.device_list.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,9 +17,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.luxrobo.ble_simulator.feature.device_list.R
+import com.luxrobo.designsystem.theme.LuxColor.Blue01
+import com.luxrobo.designsystem.theme.LuxColor.White
 import com.luxrobo.designsystem.theme.LuxTheme
 import com.luxrobo.device_list.viewModel.BleDeviceListIntent
 
@@ -45,16 +50,23 @@ fun BasicDialog(
                 .padding(24.dp)
         ) {
             Column(
-                horizontalAlignment = Alignment.CenterHorizontally
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
             ) {
                 Text(
                     text = isDialogTitle,
+                    textAlign = TextAlign.Center,
                     style = LuxTheme.typography.titleLargeB,
                     modifier = Modifier.padding(bottom = 16.dp)
                 )
 
                 Button(
                     modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(12.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = White,
+                        containerColor = Blue01,
+                    ),
                     onClick = { onDismissRequest(BleDeviceListIntent.DismissDialog) }
                 ) {
                     Text(

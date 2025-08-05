fdd64318dc81a08ae0ec74a66da61b7d32386d04
ansehun <ansehoon1999@gmail.com>
2025-08-05 11:59:00 +0900

    refactor: renewal card design

--- feature/device-list/src/main/java/com/luxrobo/device_list/ui/LazyListItem.kt
+++ feature/device-list/src/main/java/com/luxrobo/device_list/ui/LazyListItem.kt
@@ -1,14 +1,17 @@
 package com.luxrobo.device_list.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -25,10 +28,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.luxrobo.ble_simulator.feature.device_list.R
 import com.luxrobo.designsystem.component.BasicCard
+import com.luxrobo.designsystem.theme.LuxColor.Blue01
+import com.luxrobo.designsystem.theme.LuxColor.White
 import com.luxrobo.designsystem.theme.LuxTheme
 import com.luxrobo.device_list.viewModel.BleDeviceListIntent
 import com.luxrobo.model.BleDeviceConnection
-import kotlin.ranges.contains
 
 @Composable
 fun BleDeviceConnection(
@@ -36,15 +40,75 @@ fun BleDeviceConnection(
     handleIntent: (BleDeviceListIntent) -> Unit
 ) {
     BasicCard {
-        Row {
-            Row(
-                modifier = Modifier.padding(16.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
+        Row(
+            modifier = Modifier
+                .background(Color.White)
+                .padding(
+                    horizontal = 16.dp,
+                    vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+
+            Column(
+                modifier = Modifier.weight(1f)
             ) {
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                Text(
+                    style = LuxTheme.typography.titleLargeB,
+                    color = Color.Black,
+                    textAlign = TextAlign.Center,
+                    text = uiState.name,
+                    fontSize = 16.sp,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
+
+                Spacer(Modifier.height(4.dp))
+
+                Text(
+                    style = LuxTheme.typography.bodyLargeR,
+                    color = Color.Gray,
+                    fontSize = 11.sp,
+                    text = uiState.deviceId,
+                    overflow = TextOverflow.Ellipsis,
+                    minLines = 2,
+                    maxLines = 2
+                )
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    modifier = Modifier
+                        .testTag("연결 버튼"),
+                    enabled = !uiState.isConnecting,
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = White,
+                        containerColor = Blue01,
+                        disabledContentColor = Color.White,
+                        disabledContainerColor = Color.Gray
+                    ),
+                    shape = RoundedCornerShape(12.dp),
+                    contentPadding = PaddingValues(horizontal = 16.dp, vertical = 0.dp),
+                    onClick = {
+                        handleIntent(BleDeviceListIntent.SelectDevice(deviceConnection = uiState))
+                    }
+                ) {
+                    Text(
+                        fontSize = 12.sp,
+                        style = LuxTheme.typography.bodyLargeR,
+                        textAlign = TextAlign.Center,
+                        text = stringResource(R.string.bluetooth_list_connecting_status)
+                    )
+                }
+
+                Spacer(Modifier.height(4.dp))
+
+                Row (
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
 
                     Image(
@@ -59,65 +123,16 @@ fun BleDeviceConnection(
                         contentDescription = null
                     )
 
+                    Spacer(modifier = Modifier.width(4.dp))
+
                     Text(
                         style = LuxTheme.typography.bodyLargeR,
                         text = uiState.rssi.toString()
-                    )
-                }
-
-                Spacer(modifier = Modifier.width(16.dp))
-
-                Column(
-                    modifier = Modifier.weight(1f)
-                ) {
-                    Text(
-                        style = LuxTheme.typography.titleLargeB,
-                        color = Color.Black,
-                        textAlign = TextAlign.Center,
-                        text = uiState.name,
-                        overflow = TextOverflow.Ellipsis,
-                        maxLines = 1
-                    )
-
-                    Text(
-                        style = LuxTheme.typography.bodyLargeR,
-                        color = Color.Gray,
-                        text = uiState.deviceId,
-                        overflow = TextOverflow.Ellipsis,
-                        minLines = 2,
-                        maxLines = 2
-                    )
-                }
-
-                Spacer(modifier = Modifier.width(16.dp))
-
-                Button(
-                    modifier = Modifier
-                        .testTag("연결 버튼"),
-                    enabled = !uiState.isConnecting,
-                    colors = ButtonDefaults.buttonColors(
-                        contentColor = Color.Black,
-                        containerColor = Color.Green,
-                        disabledContentColor = Color.White,
-                        disabledContainerColor = Color.Gray
-                    ),
-                    contentPadding = PaddingValues(horizontal = 8.dp, vertical = 0.dp),
-                    onClick = {
-                        handleIntent(BleDeviceListIntent.SelectDevice(deviceConnection = uiState))
-                    }
-                ) {
-                    Text(
-                        fontSize = 10.sp,
-                        style = LuxTheme.typography.bodyLargeR,
-                        color = Color.LightGray,
-                        textAlign = TextAlign.Center,
-                        text = stringResource(R.string.bluetooth_list_connecting_status)
                     )
                 }
             }
 
 
         }
-
     }
 }

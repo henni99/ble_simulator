1ec69648852110bb37470e29c92eadaa1bc89269
ansehun <ansehoon1999@gmail.com>
2025-08-06 17:01:03 +0900

    refactor: refactoring data transfer screen ui design

--- feature/data-transfer/src/main/java/com/luxrobo/data_transfer/ui/TextField.kt
+++ feature/data-transfer/src/main/java/com/luxrobo/data_transfer/ui/TextField.kt
@@ -4,11 +4,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
@@ -16,13 +16,11 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
@@ -32,8 +30,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.luxrobo.designsystem.component.VerticalSpacer
+import com.luxrobo.designsystem.theme.LuxColor.Blue01
 import com.luxrobo.designsystem.theme.LuxTheme
 
 @Composable
@@ -49,7 +51,13 @@ fun ScrollableReadOnlyTextField(
     }
 
     Text(
+        modifier = Modifier
+            .fillMaxWidth(),
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        textAlign = TextAlign.Start,
         text = title,
+        fontSize = 16.sp,
         style = LuxTheme.typography.titleLargeB
     )
 
@@ -60,7 +68,7 @@ fun ScrollableReadOnlyTextField(
             .fillMaxWidth()
             .clip(RoundedCornerShape(12.dp)) // 둥근 테두리
             .background(Color.White) // 배경색
-            .border(1.dp, Color.Gray, RoundedCornerShape(12.dp)) // 테두리
+            .border(1.dp, Blue01, RoundedCornerShape(12.dp)) // 테두리
             .padding(12.dp) // 내부 패딩
             .verticalScroll(scrollState)
     ) {
@@ -76,58 +84,77 @@ fun ScrollableReadOnlyTextField(
 
 @Composable
 fun MessageInputRow(
+    modifier: Modifier = Modifier,
     text: String,
     onTextChange: (String) -> Unit,
     onSend: (String) -> Unit
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
     val focusManager = LocalFocusManager.current
+
     Row(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
+            .wrapContentHeight()
             .imePadding()
             .background(Color.White)
             .padding(8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        TextField(
-            modifier = Modifier.weight(1f),
-            value = text,
-            onValueChange = onTextChange,
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Send),
-            keyboardActions = KeyboardActions(
-                onSend = {
-                    if (text.isNotBlank()) {
-                        onSend(text)
-                        onTextChange("")
-                        keyboardController?.hide()
-                        focusManager.clearFocus(force = true)
-                    }
-                }
-            ),
-            colors = TextFieldDefaults.colors(
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                disabledIndicatorColor = Color.Transparent
-            )
-        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFFF0F0F0), shape = RoundedCornerShape(8.dp))
+                .padding(horizontal = 12.dp, vertical = 8.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                BasicTextField(
+                    value = text,
+                    onValueChange = onTextChange,
+                    singleLine = true,
+                    textStyle = LocalTextStyle.current.copy(
+                        color = Color.Black,
+                        fontSize = 16.sp
+                    ),
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Send
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onSend = {
+                            if (text.isNotBlank()) {
+                                onSend(text)
+                                onTextChange("")
+                                keyboardController?.hide()
+                                focusManager.clearFocus(force = true)
+                            }
+                        }
+                    ),
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(end = 8.dp)
 
-        IconButton(
-            modifier = Modifier.height(40.dp),
-            onClick = {
-                if (text.isNotBlank()) {
-                    onSend(text)
-                    onTextChange("")
-                    keyboardController?.hide()
-                    focusManager.clearFocus(force = true)
+                )
+
+                IconButton(
+                    modifier = Modifier.size(20.dp),
+                    onClick = {
+                        if (text.isNotBlank()) {
+                            onSend(text)
+                            onTextChange("")
+                            keyboardController?.hide()
+                            focusManager.clearFocus(force = true)
+                        }
+                    }
+                ) {
+                    Icon(
+                        tint = Blue01,
+                        imageVector = Icons.AutoMirrored.Filled.Send,
+                        contentDescription = "Send"
+                    )
                 }
             }
-        ) {
-            Icon(
-                imageVector = Icons.Default.Done,
-                contentDescription = "전송"
-            )
         }
     }
 }

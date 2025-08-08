faa6100eb50a3dd054cdf78cc31ede3993190652
ansehun <ansehoon1999@gmail.com>
2025-08-08 16:57:53 +0900

    chore: add annotation for each function in domain module

--- core/domain/src/main/java/com/luxrobo/domain/usecase/GetBleDeviceConnectionsUseCase.kt
+++ core/domain/src/main/java/com/luxrobo/domain/usecase/GetBleDeviceConnectionsUseCase.kt
@@ -6,6 +6,14 @@ import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
+/**
+ * GetBleDeviceConnectionsUseCase
+ *
+ * BLE 디바이스 연결 목록을 가져오는 UseCase 클래스.
+ * 내부에서 BleDeviceConnectionRepository의 getBleDeviceConnections 메서드를 호출하여
+ * 연결된 BLE 디바이스 리스트를 Flow 형태로 반환합니다.
+ */
+
 @Singleton
 class GetBleDeviceConnectionsUseCase @Inject constructor(
     private val bleRepository: BleDeviceConnectionRepository
--- core/domain/src/main/java/com/luxrobo/domain/usecase/GetMessageUseCase.kt
+++ core/domain/src/main/java/com/luxrobo/domain/usecase/GetMessageUseCase.kt
@@ -1,19 +1,24 @@
 package com.luxrobo.domain.usecase
 
 import com.luxrobo.domain.repository.BleTransferRepository
-import com.luxrobo.model.BleDeviceConnection
 import com.luxrobo.model.BleDeviceInfo
 import com.luxrobo.model.Message
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
+/**
+ * GetMessageUseCase
+ *
+ * BLE 기기 정보를 받아서 메시지를 비동기 스트림(Flow)으로 제공하는 UseCase 클래스.
+ * 내부에서 BleTransferRepository의 getMessage 메서드를 호출하여 데이터 획득.
+ */
+
 @Singleton
 class GetMessageUseCase @Inject constructor(
     private val bleTransferRepository: BleTransferRepository
-){
+) {
     operator fun invoke(bleDeviceInfo: BleDeviceInfo): Flow<Message> {
         return bleTransferRepository.getMessage(bleDeviceInfo)
     }
-
 }

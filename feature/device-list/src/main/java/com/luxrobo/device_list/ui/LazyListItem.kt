922b808011e102c3d39f42a5ac915cb3af2ba12f
ansehun <ansehoon1999@gmail.com>
2025-08-08 14:09:26 +0900

    fix: item padding value

--- feature/device-list/src/main/java/com/luxrobo/device_list/ui/LazyListItem.kt
+++ feature/device-list/src/main/java/com/luxrobo/device_list/ui/LazyListItem.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -47,7 +49,7 @@ fun BleDeviceConnection(
                 .background(Color.White)
                 .padding(
                     horizontal = 16.dp,
-                    vertical = 8.dp),
+                    vertical = 16.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center
         ) {
@@ -85,7 +87,8 @@ fun BleDeviceConnection(
             ) {
                 Button(
                     modifier = Modifier
-                        .testTag("연결 버튼"),
+                        .testTag("연결 버튼")
+                        .wrapContentHeight(),
                     enabled = !uiState.isConnecting,
                     colors = ButtonDefaults.buttonColors(
                         contentColor = White,
@@ -114,7 +117,7 @@ fun BleDeviceConnection(
                 ) {
 
                     Image(
-                        modifier = Modifier.size(24.dp),
+                        modifier = Modifier.size(20.dp),
                         painter = painterResource(
                             when {
                                 uiState.rssi >= -70 -> R.drawable.signal_strong
@@ -129,6 +132,7 @@ fun BleDeviceConnection(
 
                     Text(
                         style = LuxTheme.typography.bodyLargeR,
+                        fontSize = 14.sp,
                         text = uiState.rssi.toString()
                     )
                 }
